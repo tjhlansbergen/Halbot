6f8e133dd21d778505e20c94f6f01ce4d087d9a4
tjhlansbergen <takolansbergen@xs4all.nl>
2021-04-23 16:16:27 +0200

    Fix map, add link to CityStrides

--- Halbot/Models/MapModel.cs
+++ Halbot/Models/MapModel.cs
@@ -10,16 +10,26 @@ namespace Halbot.Models
         public List<HalbotActivity> Activities { get; private set; }
         public List<HalbotActivity> Geos { get; set; }
 
+        private readonly int _filterDecimals = 2;
+        // decimal places  degrees distance
+        //1	0.1	11.1 km
+        //2	0.01	1.11 km
+        //3	0.001	111 m
+ 
+
         //constructor
         public MapModel(List<HalbotActivity> activities)
         {
             Activities = activities;
-
-            //filter out activities without location
+            
             Geos = new List<HalbotActivity>();
-            foreach (var item in Activities)
+            foreach (var item in Activities.OrderByDescending(a => a.Date))     // order lets recent activities overwrite older ones on the map
             {
-                if (item.Lat != 0 && item.Lng != 0)
+                //filter out activities without location
+                if (item.Lat == 0 || item.Lng == 0) continue;
+
+                // add, while preventing items close together
+                if (!Geos.Any(i => Math.Round(i.Lat, _filterDecimals) == Math.Round(item.Lat, _filterDecimals) && Math.Round(i.Lng, _filterDecimals) == Math.Round(item.Lng, _filterDecimals)))
                 {
                     Geos.Add(item);
                 }
