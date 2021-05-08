e218bdf46fc798c155d5230c689c8249800f0355
tjhlansbergen <takolansbergen@xs4all.nl>
2021-05-08 16:07:47 +0200

    Rework Metrics chart into Effort chart

--- Halbot/Charts/SpiderChart.cs
+++ Halbot/Charts/SpiderChart.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using Halbot.Code.Charts;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Halbot.Code.Charts;
-using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 
 namespace Halbot.Charts
 {
@@ -17,32 +15,20 @@ namespace Halbot.Charts
 
     public struct DataItem
     {
-        public double A;
-        public double B;
-        public double C;
-        public double D;
-
-        public string FillColor;
-        public string StrokeColor;
+        public int Value { get; set; }
+        public DateTime Date { get; set; }
     }
 
     public class DataSet
     {
-        // a list of dataitems
-        public List<List<DataItem>> Rows { get; private set; }
-        public string Name { get; private set; }
+        // a list of data items
+        public List<DataItem> Items { get; set; }
+        public string Name { get; }
 
         // constructor
         public DataSet(string name)
         {
             Name = name;
-            Rows = new List<List<DataItem>>();
-        }
-
-        // setter for adding data
-        public void AddRow(List<DataItem> row)
-        {
-            Rows.Add(row);
         }
     }
 
@@ -55,19 +41,17 @@ namespace Halbot.Charts
         public int Columns { get; set; }
         public DataSet Data { get; set; }
 
-        public SpiderChart(string name, int width, int height, int radius, int rows, int columns)
+        public SpiderChart(string name, int width, int rows, DataSet data)
         {
             Name = name;
             Width = width;
-            Height = height;
-            Radius = radius;
             Rows = rows;
-            Columns = columns;
-        }
 
-        public void SetDataSet(DataSet data)
-        {
             Data = data;
+
+            Columns = 7;
+            Radius = Width / Columns / 2;
+            Height = Rows * Radius * 2;
         }
 
         public override string CreateHTML()
@@ -89,7 +73,7 @@ namespace Halbot.Charts
 
             // CIRCLES
             html.AppendLine("// CIRCLES");
-            html.AppendLine("ctx.strokeStyle = \"#f0f0f0\";");
+            html.AppendLine("ctx.strokeStyle = \"#f2f2f2\";");
 
             for (var i = 0; i < Rows; i++)
             {
@@ -97,7 +81,7 @@ namespace Halbot.Charts
                 {
                     html.AppendLine("ctx.beginPath();");
 
-                    foreach (var rad in new List<int> { 60, 40, 20 })
+                    foreach (var rad in new List<int> { 60, 45, 30, 15 })
                     {
                         html.AppendLine($"ctx.arc({Radius + (j * Radius * 2)}, {Radius + (i * Radius * 2)}, {rad}, 0, 2 * Math.PI);");
                         html.AppendLine("ctx.stroke();");
@@ -111,49 +95,50 @@ namespace Halbot.Charts
 
 
 
-            var aMinMax = new MinMax{ Max = Data.Rows.SelectMany(r => r.Select(c => c.A)).Max(), Min = Data.Rows.SelectMany(r => r.Select(c => c.A)).Min() };
-            var bMinMax = new MinMax { Max = Data.Rows.SelectMany(r => r.Select(c => c.B)).Max(), Min = Data.Rows.SelectMany(r => r.Select(c => c.B)).Min() };
-            var cMinMax = new MinMax { Max = Data.Rows.SelectMany(r => r.Select(c => c.C)).Max(), Min = Data.Rows.SelectMany(r => r.Select(c => c.C)).Min() };
-            var dMinMax = new MinMax { Max = Data.Rows.SelectMany(r => r.Select(c => c.D)).Max(), Min = Data.Rows.SelectMany(r => r.Select(c => c.D)).Min() };
-
+            var aMinMax = new MinMax{ Max = Data.Items.Select(r => r.Value).Max(), Min = Data.Items.Select(r => r.Value).Min() };
             var aFactor = 70 / (aMinMax.Max - aMinMax.Min);
-            var bFactor = 70 / (bMinMax.Max - bMinMax.Min);
-            var cFactor = 70 / (cMinMax.Max - cMinMax.Min);
-            var dFactor = 70 / (dMinMax.Max - dMinMax.Min);
-            
-            // foreach row
-            for (int i = 0; i < Data.Rows.Take(Rows).ToArray().Length; i++)
+
+            // get the most recent monday
+            var lastMonday = DateTime.UtcNow;
+            while (lastMonday.DayOfWeek != DayOfWeek.Monday)
             {
-                var rowItems = Data.Rows[i].Take(Columns).ToArray();
+                lastMonday = lastMonday.AddDays(-1);
+            }
+
+            // foreach row
+            for (int i = 0; i < Rows; i++)
+            {
 
                 html.AppendLine($"// Row: {i}");
 
                 //foreach column
-                //foreach (var dataItem in rowItems)
-                for (int j = 0; j < rowItems.Count(); j++)
+                for (int j = 0; j < Columns; j++)
                 {
-                    var item = rowItems[j];
+                    html.AppendLine($"// Column: {j}");
+
+                    var days = (0 - (i * Columns)) + j;
+                    var day = lastMonday.AddDays(days).Date;
+                    var items = Data.Items.Where(itm => itm.Date.Date == day).OrderByDescending(itm => itm.Value).ToList();
+
+                    if (!items.Any()) continue;
 
                     var x = Radius + (j * Radius * 2);
                     var y = Radius + (i * Radius * 2);
 
-                    var a = Convert.ToInt32((item.A - aMinMax.Min) * aFactor) + 5;
-                    var b = Convert.ToInt32((item.B - bMinMax.Min) * bFactor) + 5;
-                    var c = Convert.ToInt32((item.C - cMinMax.Min) * cFactor) + 5;
-                    var d = Convert.ToInt32((item.D - dMinMax.Min) * dFactor) + 5;
+                    for (int k = 0; k < items.Count; k++)
+                    {
+                        x += k * 20;
 
-                    html.AppendLine($"ctx.fillStyle = \"{item.FillColor}\";");
-                    html.AppendLine($"ctx.strokeStyle = \"{item.StrokeColor}\";");
-                    html.AppendLine("ctx.beginPath();");
+                        var a = Convert.ToInt32((items[k].Value - aMinMax.Min) * aFactor) + 5;
 
-                    html.AppendLine($"ctx.moveTo({x}, {y} - {a});");     // move up by A (top)
-                    html.AppendLine($"ctx.lineTo({x} + {b}, {y});");     // line to B (right)
-                    html.AppendLine($"ctx.lineTo({x}, {y} + {c});");     // line to C (bottom)
-                    html.AppendLine($"ctx.lineTo({x} - {d}, {y});");     // line to D (left)
-                    html.AppendLine($"ctx.lineTo({x}, {y} - {a});");     // back to start (top)
-
-                    html.AppendLine("ctx.fill();");
-                    html.AppendLine("ctx.stroke();");
+                        html.AppendLine($"ctx.fillStyle = \"{GetColors(items.First().Date.DayOfWeek).Item1}\";");
+                        html.AppendLine($"ctx.strokeStyle = \"{GetColors(items.First().Date.DayOfWeek).Item2}\";");
+                        html.AppendLine("ctx.beginPath();");
+                        html.AppendLine("ctx.lineWidth = 2;");
+                        html.AppendLine($"ctx.arc({x}, {y}, {a}, 0, 2 * Math.PI);");
+                        html.AppendLine("ctx.fill();");
+                        html.AppendLine("ctx.stroke();");
+                    }
                 }
             }
 
@@ -162,6 +147,29 @@ namespace Halbot.Charts
 
 
             return html.ToString();
+        }
+
+        private static Tuple<string, string> GetColors(DayOfWeek day)
+        {
+            switch (day)
+            {
+                case DayOfWeek.Monday:
+                    return new Tuple<string, string>("#deede4", "#89BD9E");
+                case DayOfWeek.Tuesday:
+                    return new Tuple<string, string>("#b4e4fd", "#058ED9");
+                case DayOfWeek.Wednesday:
+                    return new Tuple<string, string>("#f6d5df", "#8B1E3F");
+                case DayOfWeek.Thursday:
+                    return new Tuple<string, string>("#f9ebd2", "#F0C987");
+                case DayOfWeek.Friday:
+                    return new Tuple<string, string>("#f2d9f1", "#3C153B");
+                case DayOfWeek.Saturday:
+                    return new Tuple<string, string>("#b8f9f8", "#08605F");
+                case DayOfWeek.Sunday:
+                    return new Tuple<string, string>("#f7d7d4", "#DB4C40");
+                default:
+                    return new Tuple<string, string>("grey", "black");
+            }
         }
     }
 }
--- Halbot/Models/ChartsMetricsModel.cs
+++ Halbot/Models/ChartsMetricsModel.cs
@@ -18,57 +18,15 @@ namespace Halbot.Models
             Activities = activities;
 
             // create chart
-            SpiderChart = new SpiderChart("Metrics", 900, 1500, 75, 10, 6);
-
-            
-            var data = new DataSet("activities");
-
-            for (int i = 0; i < 10; i++)
+            var data = new DataSet("activities")
             {
-                var week = HalbotActivity.WeekOfYear(DateTime.Now) - i;
-                if (week < 1) { week += 52; }
-                var year = (week <= HalbotActivity.WeekOfYear(DateTime.Now)) ? DateTime.Now.Year : DateTime.Now.Year - 1;
+                Items = activities.Select(a => new DataItem { Value = a.Effort, Date = a.Date }).ToList()
+            };
 
-                var row = new List<DataItem>();
-
-                // get the activities for the week
-                var rowActivities = Activities.Where(a => a.Date.Year == year && a.Week == week).OrderByDescending(a => a.Date);
-
-                foreach (var a in rowActivities)
-                {
-                    row.Add(new DataItem { A = a.Distance, B = a.Speed, C = a.Heartrate, D = a.Climb, FillColor = GetColors(a.Date.DayOfWeek).Item1, StrokeColor = GetColors(a.Date.DayOfWeek).Item2 });
-                }
-
-                data.AddRow(row);
-            }
-
-
-
-            SpiderChart.SetDataSet(data);
-
+            SpiderChart = new SpiderChart("Metrics", 900,10, data);
         }
 
-        private static Tuple<string, string> GetColors(DayOfWeek day)
-        {
-            switch (day)
-            {
-                case DayOfWeek.Monday:
-                    return new Tuple<string, string>("#FC600A", "#9D3802");
-                case DayOfWeek.Tuesday:
-                    return new Tuple<string, string>("#FCCB1A", "#B08A03");
-                case DayOfWeek.Wednesday:
-                    return new Tuple<string, string>("#98CA32", "#59761E");
-                case DayOfWeek.Thursday:
-                    return new Tuple<string, string>("#559E54", "#305A30");
-                case DayOfWeek.Friday:
-                    return new Tuple<string, string>("#1258DC", "#0A337F");
-                case DayOfWeek.Saturday:
-                case DayOfWeek.Sunday:
-                    return new Tuple<string, string>("#AE0D7A", "#510639");
-                default:
-                    return new Tuple<string, string>("grey", "black");
-            }
-        }
+
 
     }
 }
