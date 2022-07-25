dbc5ca67bdfcb061c0d30855664c5cb1d0c75a90
tjhlansbergen <takolansbergen@xs4all.nl>
2022-07-25 16:32:21 +0200

    Add /api/activities

---
+++ Halbot/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Halbot.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Halbot.Controllers
+{
+    [Route("api/activities")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly DatabaseContext _dbcontext = new DatabaseContext();
+
+        // GET: api/activities
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return ActivityCache.Get(_dbcontext).Select(a => a.Id.ToString()).ToArray();
+        }
+
+        // GET api/activities/5
+        [HttpGet("{id}")]
+        public string Get(long id)
+        {
+            var obj = ActivityCache.Get(_dbcontext).Single(a => a.Id == id);
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
