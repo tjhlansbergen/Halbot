0a85dede443935e568cba4d497443af86bbcdc99
Tako Lansbergen <lansta@uptrends.com>
2025-03-14 12:07:08 +0100

    Allow editing and deleting of workouts, update their displayal

--- Halbot/Controllers/WorkoutController.cs
+++ Halbot/Controllers/WorkoutController.cs
@@ -1,6 +1,7 @@
 ﻿using Halbot.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace Halbot.Controllers
 {
@@ -9,5 +10,55 @@ namespace Halbot.Controllers
         private readonly DatabaseContext _dbcontext = new DatabaseContext();
         private readonly Logger _logger = new Logger();
 
+        public IActionResult Workout(long id)
+        {
+            return View("EditWorkout", WorkoutCache.Get(_dbcontext).Single(a => a.Id == id));
+        }
+
+        public IActionResult Save(string notes, int minutes, DateTime date, long id)
+        {
+            try
+            {
+                var record = _dbcontext.WorkoutRecords.Single(r => r.Id == id);
+                record.Notes = notes;
+                record.Minutes = minutes;
+                record.Date = date;
+
+                _dbcontext.SaveChanges();
+
+                WorkoutCache.InvalidateCache();
+
+                _logger.Log(LogSeverityLevel.Info, $"Edited workout with ID: {id}");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogSeverityLevel.Error, $"Error editing workout with ID: {id}");
+                _logger.Log(LogSeverityLevel.Error, ex.Message);
+                return RedirectToAction("Log", "Home");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Delete(long id)
+        {
+            try
+            {
+                _dbcontext.WorkoutRecords.Remove(_dbcontext.WorkoutRecords.Single(r => r.Id == id));
+                _dbcontext.SaveChanges();
+
+                WorkoutCache.InvalidateCache();
+
+                _logger.Log(LogSeverityLevel.Info, $"Deleted workout with ID: {id}");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogSeverityLevel.Error, $"Error deleting workout with ID: {id}");
+                _logger.Log(LogSeverityLevel.Error, ex.Message);
+                return RedirectToAction("Log", "Home");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
